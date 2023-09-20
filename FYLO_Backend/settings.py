526bfced52cbff0955a2c47af251567610aebbff
malcolm-bowers <101752045+malcolm-bowers@users.noreply.github.com>
2023-09-20 17:16:15 -0500

    Update settings.py

--- FYLO_Backend/settings.py
+++ FYLO_Backend/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
