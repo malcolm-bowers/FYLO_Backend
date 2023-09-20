247f8edc33e6bf73267db6e5ff2cf418dcd7f47f
malcolm-bowers <101752045+malcolm-bowers@users.noreply.github.com>
2023-09-20 17:12:14 -0500

    Update settings.py

--- FYLO_Backend/settings.py
+++ FYLO_Backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b38af946-ecc6-412f-8873-1bc448e869cb'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
